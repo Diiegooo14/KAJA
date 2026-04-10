2a4a6049ccb0627f609464f1ebd8df3710ecc08f
Diiegooo1414 <diprilu2006@gmail.com>
2026-04-10 12:56:02 +0200

    Estructura base del config.php y archivo .env del frontend

---
+++ backend/Config/config.php
@@ -0,0 +1,10 @@
+<?php
+class Config
+{
+    public const HOST = ''; //host de la base de datos
+    public const DATABASE = ''; //nombre de la base de datos
+    public const USERNAME = ''; //usuario con el que accedemos
+    public const PASSWORD = ''; //contraseña del usuario
+    public const PORT = ''; //puerto en el que escucha MariaDB o MySQL
+    public const CHARSET = 'utf8mb4'; //define el encoding que se va a usar para la conexión
+}
