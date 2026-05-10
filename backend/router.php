1095da70c7af11d01e5e82f937bf7bba85939bf7
Diiegooo1414 <diprilu2006@gmail.com>
2026-05-10 17:42:20 +0200

    cambio de servidor del backend

--- backend/router.php
+++ backend/router.php
@@ -1,12 +1,20 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $file = __DIR__ . '/Api' . $uri . '.php';
 
 if (file_exists($file)) {
     require $file;
 } else {
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json; charset=utf-8');
     http_response_code(404);
     echo json_encode(['error' => 'Ruta no encontrada']);
 }
